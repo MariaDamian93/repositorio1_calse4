7ce252f85aed432076d8c553a958ff28a3615df7
USUARIO <USUARIO@DESKTOP-3QR18Q3>
2025-02-12 20:55:56 -0500

    Carga inicial

--- src/Programas/Programa1.java
+++ src/Programas/Programa1.java
@@ -6,5 +6,6 @@ import java.util.Scanner;
 public class Programa1 {
     public static void main(String arg[]){
         System.out.println("hola mundo");
+        System.out.println("bienvenidos al curso de DevOps");
     }
 }
